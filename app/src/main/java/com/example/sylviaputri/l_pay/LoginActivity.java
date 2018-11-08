f9976f61a197f750e4b51d49688978247626504b
yogarizky <yoga.rizky@ti.ukdw.ac.id>
2018-11-08 11:30:21 +0700

    Merge branch 'master' into yoga

--- app/src/main/java/com/example/sylviaputri/l_pay/LoginActivity.java
+++ app/src/main/java/com/example/sylviaputri/l_pay/LoginActivity.java
@@ -2,12 +2,21 @@ package com.example.sylviaputri.l_pay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+    public FrameLayout frmLoginBackground;
+    public ImageView imgLoginLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        frmLoginBackground = findViewById(R.id.frmLoginBackground);
+        imgLoginLogo = findViewById(R.id.imgLoginLogo);
+
+
     }
 }

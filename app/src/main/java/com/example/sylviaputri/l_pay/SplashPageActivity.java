6c05217d2da241f59cbb9f1630202c904a9c3b9c
sylviaputri <sylvi.bluemochi@gmail.com>
2018-11-10 00:56:54 +0700

    tambah splashPage
    otw slider page

---
+++ app/src/main/java/com/example/sylviaputri/l_pay/SplashPageActivity.java
@@ -0,0 +1,25 @@
+package com.example.sylviaputri.l_pay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashPageActivity extends AppCompatActivity {
+    private  static int SPLASH_TIME_OUT = 3500;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashPageActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}

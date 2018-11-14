98c8262ccbbce1e76450d14e986a46f5cb94119d
sylviaputri <sylvi.bluemochi@gmail.com>
2018-11-14 08:26:33 +0700

    update

--- app/src/main/java/com/example/sylviaputri/l_pay/WithdrawActivity.java
+++ app/src/main/java/com/example/sylviaputri/l_pay/WithdrawActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class WithdrawActivity extends AppCompatActivity {
     public ImageButton btnWithdrawBack;
+    public Button btnWithdrawWithdraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +17,17 @@ public class WithdrawActivity extends AppCompatActivity {
         setContentView(R.layout.activity_withdraw);
 
         btnWithdrawBack = (ImageButton) findViewById(R.id.btnWithdrawBack);
+        btnWithdrawWithdraw = (Button) findViewById(R.id.btnWithdrawWithdraw);
 
         btnWithdrawBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WithdrawActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWithdrawWithdraw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(WithdrawActivity.this, HomeActivity.class);

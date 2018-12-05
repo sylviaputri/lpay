87060bf1de67af34597c1fd9e4ba7e2521556dc8
zefanyadavids <zefanyadavid0203@gmail.com>
2018-12-05 09:21:31 +0700

    Dinamic Home Page
    Tampil nama, email, nomor, saldo dari fire base..

--- app/src/main/java/com/example/sylviaputri/l_pay/HomeActivity.java
+++ app/src/main/java/com/example/sylviaputri/l_pay/HomeActivity.java
@@ -11,6 +11,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sylviaputri.l_pay.Model.AuthUser;
+import com.example.sylviaputri.l_pay.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class HomeActivity extends AppCompatActivity {
     public ImageButton btnHomeTopUp;
     public ImageButton btnHomeWithdraw;
@@ -20,6 +49,17 @@ public class HomeActivity extends AppCompatActivity {
     public TextView txtHomeHome;
     public TextView txtHomeHistory;
     public TextView txtHomeSetting;
+    private TextView txtHomeSaldo;
+    private TextView txtHomeNama;
+    private TextView txtHomeNoTelp;
+    private TextView txtHomeEmail;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser curUser = mAuth.getCurrentUser();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("User").child(curUser.getUid());
+
+    private ValueEventListener valueEvent;
+    private User user;
 
     private static long back_pressed ;
 
@@ -28,6 +68,33 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //variable declaration
+        txtHomeSaldo = (TextView) findViewById(R.id.txtHomeSaldo);
+        txtHomeNama = (TextView) findViewById(R.id.txtHomeNama);
+        txtHomeNoTelp = (TextView) findViewById(R.id.txtHomeNoTelp);
+        txtHomeEmail = (TextView) findViewById(R.id.txtHomeEmail);
+
+        //view from firebase
+        txtHomeNama.setText(curUser.getDisplayName());
+        txtHomeEmail.setText(curUser.getEmail());
+        txtHomeNoTelp.setText(curUser.getPhoneNumber());
+
+        valueEvent = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+                //user = new User(Double.valueOf(dataSnapshot.child("saldo").getValue().toString())); //pake constructor -> User(Double saldo)
+                txtHomeSaldo.setText("Rp. "+ user.getSaldo().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.addValueEventListener(valueEvent);
+
+        //variable declaration
         btnHomeTopUp = (ImageButton)findViewById(R.id.btnHomeTopUp);
         btnHomeWithdraw = (ImageButton)findViewById(R.id.btnHomeWithdraw);
         btnHomeScanQR = (ImageButton) findViewById(R.id.btnHomeScanQR);
@@ -36,6 +103,7 @@ public class HomeActivity extends AppCompatActivity {
         txtHomeSetting = (TextView) findViewById(R.id.txtHomeSetting);
         txtHomeHistory = (TextView) findViewById(R.id.txtHomeHistory);
 
+        //on click function
         btnHomeTopUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
